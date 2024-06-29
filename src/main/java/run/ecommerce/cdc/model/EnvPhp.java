55ae6625b1cb8dfb67ac0b32681a5a03c25a0a90
Kirill Morozov <kir.morozov@gmail.com>
2024-06-29 00:13:02 -0400

    Cleanup

--- src/main/java/run/ecommerce/cdc/model/EnvPhp.java
+++ src/main/java/run/ecommerce/cdc/model/EnvPhp.java
@@ -6,19 +6,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import org.json.JSONObject;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 @Component
 public class EnvPhp {
