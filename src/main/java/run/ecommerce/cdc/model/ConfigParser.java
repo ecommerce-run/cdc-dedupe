274316bc96e78a1e57e0fddfff81395c4a79f05d
Kirill Morozov <kir.morozov@gmail.com>
2024-07-29 02:15:29 -0400

    Fixed up with a Spring annotation

--- src/main/java/run/ecommerce/cdc/model/ConfigParser.java
+++ src/main/java/run/ecommerce/cdc/model/ConfigParser.java
@@ -2,12 +2,15 @@ package run.ecommerce.cdc.model;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
+import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+@Component
 public class ConfigParser {
 
     public record Config(
@@ -41,6 +44,7 @@ public class ConfigParser {
             int time
     ) {}
 
+    @RegisterReflectionForBinding(Config.class)
     public static Config loadConfig(String configPath) throws IOException {
 
         ObjectMapper mapper = new ObjectMapper();
